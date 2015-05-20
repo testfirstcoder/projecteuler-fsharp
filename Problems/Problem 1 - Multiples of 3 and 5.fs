cd5c0b28bccd2dfea56f7131c066eb236e12f463
Andriy Tolstoy <andriy.tolstoy@outlook.com>
2015-05-20 17:01:41 +0200

    Problem 1 - Multiples of 3 and 5

---
+++ Problems/Problem 1 - Multiples of 3 and 5.fs
@@ -0,0 +1,13 @@
+﻿module ``Problem 1 - Multiples of 3 and 5``
+
+open NUnit.Framework
+open Swensen.Unquote
+
+let sumOfMultiples predicate n  = [|1..n - 1|] |> Array.filter predicate |> Array.sum
+
+let ``sum of multiples of 3 or 5`` n = sumOfMultiples (fun x -> x % 3 = 0 || x % 5 = 0) n
+
+[<TestCase(10, 23)>]
+[<TestCase(1000, 233168)>]
+let ``sum of all the multiples of 3 or 5 for natural numbers below n``(n: int, result: int) =
+    test <@ ``sum of multiples of 3 or 5`` n = result @>
