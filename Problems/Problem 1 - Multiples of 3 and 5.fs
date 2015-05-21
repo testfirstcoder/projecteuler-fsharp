1e7a68a42a33e57bdbc1ca9968e6c4ad584f014f
testfirstcoder <andriy.tolstoy@outlook.com>
2015-05-21 14:43:29 +0200

    Problem 2 - Even Fibonacci numbers

--- Problems/Problem 1 - Multiples of 3 and 5.fs
+++ Problems/Problem 1 - Multiples of 3 and 5.fs
@@ -3,11 +3,15 @@
 open NUnit.Framework
 open Swensen.Unquote
 
-let sumOfMultiples predicate n  = [|1..n - 1|] |> Array.filter predicate |> Array.sum
+let ``Σ of multiples`` predicate n  = 
+    [|1..n - 1|] 
+    |> Array.filter predicate 
+    |> Array.sum
 
-let ``sum of multiples of 3 or 5`` n = sumOfMultiples (fun x -> x % 3 = 0 || x % 5 = 0) n
+let ``Σ of multiples of 3 or 5`` n = 
+    ``Σ of multiples`` (fun x -> x % 3 = 0 || x % 5 = 0) n
 
 [<TestCase(10, 23)>]
 [<TestCase(1000, 233168)>]
-let ``sum of all the multiples of 3 or 5 for natural numbers below n``(n: int, result: int) =
-    test <@ ``sum of multiples of 3 or 5`` n = result @>
+let ``Σ of all the multiples of 3 or 5 for natural numbers < n`` n result =
+    test <@ ``Σ of multiples of 3 or 5`` n = result @>
---
+++ Problems/Problem 2 - Even Fibonacci numbers.fs
@@ -0,0 +1,28 @@
+﻿module ``Even Fibonacci numbers``
+
+open NUnit.Framework
+open Swensen.Unquote
+
+let even x = x &&& 1 <> 1
+
+let fibonacci = 
+    let rec fibonacci (a, b) =
+        seq { 
+            yield a
+            yield! fibonacci (b, b + a) 
+        }
+
+    fibonacci(1, 2)
+
+let ``Σ Fibonacci numbers < n`` predicate n = 
+    fibonacci 
+    |> Seq.takeWhile (fun e -> e <= n) 
+    |> Seq.filter predicate
+    |> Seq.sum
+
+[<TestCase(2, 2)>]
+[<TestCase(8, 10)>]
+[<TestCase(100, 44)>]
+[<TestCase(4000000, 4613732)>]
+let ``Σ even Fibonacci numbers < n`` n sum =
+    test <@ ``Σ Fibonacci numbers < n`` even n = sum @>
