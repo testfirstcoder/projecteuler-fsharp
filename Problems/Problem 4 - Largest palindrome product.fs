2284af4e65e8ea519ce2b9f72389578a2674a6f4
testfirstcoder <andriy.tolstoy@outlook.com>
2015-05-27 15:18:18 +0200

    Problem 4 - Largest palindrome product

---
+++ Problems/Problem 4 - Largest palindrome product.fs
@@ -0,0 +1,26 @@
+﻿module ``Problem 4 - Largest palindrome product``
+
+let isPalindrome n = n |> string |> Array.ofSeq |> (fun e -> e = Array.rev e)
+
+let largestPalindrome digitNumbersCount =
+    let largestNumber = "9" |> String.replicate digitNumbersCount |> int
+    
+    [|1..largestNumber|] 
+    |> Array.collect (fun e1 -> [|for e2 in e1..largestNumber -> e1 * e2|])
+    |> Array.filter isPalindrome
+    |> Array.max
+
+open NUnit.Framework
+open Swensen.Unquote
+
+[<TestCase(2, 9009)>]
+[<TestCase(3, 906609)>]
+let ``Largest palindrome made from the product of two n-digit numbers`` digitNumbers result = 
+    test <@ largestPalindrome digitNumbers = result @>
+
+
+#if INTERACTIVE
+#time
+largestPalindrome 3
+#time
+#endif
